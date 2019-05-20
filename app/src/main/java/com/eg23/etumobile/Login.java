41a6d9e438a730d91994b3902522b6701887684f
swve <bdrswv@gmail.com>
2019-05-20 14:08:51 +0200

    Activities change + Quickfixes

--- app/src/main/java/com/eg23/etumobile/Login.java
+++ app/src/main/java/com/eg23/etumobile/Login.java
@@ -1,7 +1,14 @@
 package com.eg23.etumobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide(); // hide the title bar
+
+
+        final Button button = findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Login.this, MainActivity.class);
+                Login.this.startActivity(myIntent);
+            }
+        });
+
     }
+
 }
