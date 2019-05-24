47d27b03d2c374434cdc6ba465e18c33db2509fa
swve <bdrswv@gmail.com>
2019-05-24 15:19:16 +0200

    Paramètres & Profile ✔

--- app/src/main/java/com/eg23/etumobile/ParamsActivity.java
+++ app/src/main/java/com/eg23/etumobile/ParamsActivity.java
@@ -2,6 +2,8 @@ package com.eg23.etumobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ParamsActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class ParamsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_params);
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+
+        // List 1
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.importance,
+                android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        // End List
+
+        // List 1
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.quand,
+                android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        // End List
+
+        // List 1
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.reccurence,
+                android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        // End List
+
+        // List 1
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.lang,
+                android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        // End List
     }
 }
--- app/src/main/java/com/eg23/etumobile/ProfileActivity.java
+++ app/src/main/java/com/eg23/etumobile/ProfileActivity.java
@@ -1,7 +1,12 @@
 package com.eg23.etumobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ImageView image_btn =  findViewById(R.id.params_btn);
+        image_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ProfileActivity.this, ParamsActivity.class);
+                ProfileActivity.this.startActivity(myIntent);
+                //Toast.makeText(ProfileActivity.this, "Bienvenue à l'application étudiante de l'UTT", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
 }
