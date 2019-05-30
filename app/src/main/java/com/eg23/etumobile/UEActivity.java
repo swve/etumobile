473d8e177c35256d80584b12e98f2c30aea05e6b
swve <bdrswv@gmail.com>
2019-05-31 00:43:52 +0200

    UE infos

--- app/src/main/java/com/eg23/etumobile/UEActivity.java
+++ app/src/main/java/com/eg23/etumobile/UEActivity.java
@@ -1,7 +1,10 @@
 package com.eg23.etumobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UEActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class UEActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ue);
+
+        Button eg23_btn =  findViewById(R.id.eg23_more);
+        eg23_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent myIntent = new Intent(UEActivity.this, UEInfoActivity.class);
+                UEActivity.this.startActivity(myIntent);
+                /** Fading Transition Effect */
+                UEActivity.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                //Toast.makeText(ProfileActivity.this, "Bienvenue à l'application étudiante de l'UTT", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
